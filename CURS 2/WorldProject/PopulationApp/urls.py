cf77031319be02c10db1b5ea8d7e590e7f50a967
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-06 20:40:58 +0300

    Delete CURS 2/WorldProject directory

--- CURS 2/WorldProject/PopulationApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import choose_countries_view
-from .views import all_countries_view
-
-urlpatterns = [
-
-	path("all", all_countries_view),
-	path("choose", choose_countries_view),
-]
