fc2b945589472619569b500803bd07cc5f84ff56
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-12 16:18:58 +0300

    Categorii

--- CURS 3/Ecommerce/shop/views.py
+++ CURS 3/Ecommerce/shop/views.py
@@ -10,6 +10,17 @@ def all_products_view(request):
 	}
     return render(request, 'all_products.html', context)
 
+
 def product_details_view(request):
 	context = {}
 	return render(request, 'product_details.html', context)
+
+
+def all_categories_view(request):
+	context = {}
+	return render(request, 'all_categories.html', context)
+
+
+def category_details_view(request):
+	context = {}
+	return render(request, 'category_details.html', context)
