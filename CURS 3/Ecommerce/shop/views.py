5f56de30f002f80bb46dcd011f34d836f5790221
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-12 16:15:09 +0300

    Site Ecommerce cu produse

---
+++ CURS 3/Ecommerce/shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+def all_products_view(request):
+    products = Product.objects.all()
+    context = {
+		"products" : products
+	}
+    return render(request, 'all_products.html', context)
+
+def product_details_view(request):
+	context = {}
+	return render(request, 'product_details.html', context)
