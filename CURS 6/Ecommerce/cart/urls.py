34f08db11319103eed8745fd82697132e718edbd
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-18 13:48:08 +0300

    Delete CURS 6 directory

--- CURS 6/Ecommerce/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import cart_view, add_to_cart_view
-
-
-urlpatterns = [
-
-	path("cart", cart_view, name = "cart_url"),
-	path("add/<slug>", add_to_cart_view, name = "add_to_cart_url"),
-]
