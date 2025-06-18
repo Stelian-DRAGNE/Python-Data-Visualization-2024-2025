74273bd6183d1de824f7f0beeb25a55c03ca753d
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-18 22:58:56 +0300

    Cart

--- CURS 6/Ecommerce/cart/views.py
+++ CURS 6/Ecommerce/cart/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, redirect
-import pandas as pd
 
 # Create your views here.
 
+
+class Cart:
+	def __init__(self, request):
+		self.request = request
+
+
 def cart_view(request):
-	try:
-		df = pd.read_csv("produse_salvate.csv")
-	except:
-		df = pd.DataFrame({"produse" : []})
-	context = {"produse": df}
+
+	## TODO:  cart = Cart(request)
+	## cart.exising_products
+
+	produse_existente = request.session.get("cos", { })
+	context = {"produse":produse_existente}
 	return render(request, 'cart.html', context)
 
 
 def add_to_cart_view(request, slug):
-    print("Ar trebui adaugat in cos...", slug)
-    print("Sesiunea curenta:", request.session)
-    return redirect("cart_url")
+
+	## TODO:  cart = Cart(request)
+
+	print("Ar trebui adaugat in cos..", slug)
+	print("Sesiunea curenta:", request.session)
+	
+	print("Sesiune", request.POST)
+
+	produse_existente = request.session.get("cos", { })
+
+	quantity = request.POST.get("quantity", 1)
+	quantity = int(quantity)
+	existing_quantity = produse_existente.get(slug, 0)
+	quantity += existing_quantity
+	produse_existente[slug] =  quantity
+	request.session["cos"] = produse_existente
+
+	return redirect("cart_url")
